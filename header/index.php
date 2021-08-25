9c348691a64e517a2bd4a6c1641514cc3496d0e0
vardan <69822867+vardan-info@users.noreply.github.com>
2021-08-25 23:37:50 +0400

    email
    email

---
+++ header/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<head>
+<title>Form submission</title>
+</head>
+<body>
+
+<form action="" method="post">
+First Name: <input type="text" name="first_name"><br>
+Last Name: <input type="text" name="last_name"><br>
+Email: <input type="text" name="email"><br>
+Message:<br><textarea rows="5" name="message" cols="30"></textarea><br>
+<input type="submit" name="submit" value="Submit">
+</form>
+
+
+
+
+<?php 
+if(isset($_POST['submit'])){
+    $to = "tumanyanvardan63@gmail.com"; //  Email address
+    $from = $_POST['email']; //sender's Email address
+    $first_name = $_POST['first_name'];
+    $last_name = $_POST['last_name'];
+    $subject = "Form submission";
+    $subject2 = "Copy of your form submission";
+    $message = $first_name . " " . $last_name . " wrote the following:" . "\n\n" . $_POST['message'];
+    $message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['message'];
+
+    $headers = "From:" . $from;
+    $headers2 = "From:" . $to;
+    mail($to,$subject,$message,$headers);
+    mail($from,$subject2,$message2,$headers2); 
+    echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
+    
+    }
+?>
+
+
+</body>
+</html>
